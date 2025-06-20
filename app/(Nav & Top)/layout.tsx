ac8144bf84baf9a1b604d70fa66fc70a8033b614
albocoq <albocoq@gmail.com>
2025-06-20 11:29:31 +0200

    Remove Hotjar tracking script from Nav and Top layouts
    - Deleted the Hotjar tracking script from the head section in both layout.tsx files to streamline the component structure and improve performance.

--- app/(Nav & Top)/layout.tsx
+++ app/(Nav & Top)/layout.tsx
@@ -42,20 +42,6 @@ export default async function RootLayout({
   const locale = await getLocale();
   return (
     <html lang={locale} suppressHydrationWarning>
-      <head>
-        <Script id="hotjar-tracking" strategy="afterInteractive">
-          {`
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:6381926,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </Script>
-      </head>
       <body className={`${poppinsSans.variable} antialiased`}>
         <WebSocketInitializer token={token} />
         <TimerInitializer />
--- app/(Nav)/layout.tsx
+++ app/(Nav)/layout.tsx
@@ -40,20 +40,6 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <head>
-        <Script id="hotjar-tracking" strategy="afterInteractive">
-          {`
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:6381926,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </Script>
-      </head>
       <body className={`${poppinsSans.variable} antialiased`}>
         <UserProfileInitializer />
         <WebSocketInitializer token={token} />
